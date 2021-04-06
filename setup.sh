08d69085793981cff1c087b83a27402408a84809
Jainish-Jain <chngnddvlop@gmail.com>
2021-04-06 16:45:10 +0530

    update theme setting

--- setup.sh
+++ setup.sh
@@ -6,4 +6,11 @@ port = $PORT\n\
 enableCORS = false\n\
 headless = true\n\
 \n\
+[theme]
+primaryColor='#f63366'\n\
+backgroundColor='#0e1117'\n\
+secondaryBackgroundColor='#31333F'\n\
+textColor='#fafafa'\n\
+font='monospace'\n\
+\n\
 " > ~/.streamlit/config.toml
